76c14835dd5300d3692b12f6f5d6bf5550ab7dab
JustinParviz <jarshravan@gmail.com>
2025-01-30 16:16:47 -0500

    Implemented the Exchange Rates API and the useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import CurrencyRow from './CurrencyRow';
 
+
+const API_KEY = "c40dd10313559f62615f884eec495fa8";
+const BASE_URL = `https://api.exchangeratesapi.io/v1/latest?access_key=${API_KEY}`;
+
+
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, [])
 
   return (
     <>
