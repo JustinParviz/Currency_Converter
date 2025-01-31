e3c97dd371a25e9464cfaa0442aef1a8691bd2ab
JustinParviz <jarshravan@gmail.com>
2025-01-31 15:44:41 -0500

    Set a default selectedCurrency for each select option

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,18 @@ const BASE_URL = `https://api.exchangeratesapi.io/v1/latest?access_key=${API_KEY
 
 function App() {
   const [currencyOptions, setCurrencyOptions] = useState([])
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, setToCurrency] = useState()
+
 
   useEffect(() => {
     fetch(BASE_URL)
       .then(res => res.json())
       .then(data => {
+        const firstCurrency = Object.keys(data.rates)[0]
         setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+        setFromCurrency(data.base)
+        setToCurrency(firstCurrency)
       })
   }, [])
 
@@ -23,10 +29,12 @@ function App() {
       <h1>Currency Converter</h1>
       <CurrencyRow
         currencyOptions={currencyOptions}
+        selectedCurrency={fromCurrency}
       />
       <div className="equals">=</div>
       <CurrencyRow 
         currencyOptions={currencyOptions}
+        selectedCurrency={toCurrency}
       />
     </>
   );
