6437f3b5c8b60b257665470c96ed2356446d9446
JustinParviz <jarshravan@gmail.com>
2025-01-30 19:37:55 -0500

    Utilized the useState Hook for what the currency list is going to be

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,20 @@ const BASE_URL = `https://api.exchangeratesapi.io/v1/latest?access_key=${API_KEY
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currencyOptions, setCurrencyOptions] = useState([])
+  console.log(currencyOptions)
 
   useEffect(() => {
     fetch(BASE_URL)
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => {
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+      })
   }, [])
 
   return (
     <>
-      <h1>Convert</h1>
+      <h1>Currency Converter</h1>
       <CurrencyRow />
       <div className="equals">=</div>
       <CurrencyRow />
