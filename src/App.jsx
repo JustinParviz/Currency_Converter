8a52a75765109f23c5c2395cfdf69950a6b92683
JustinParviz <jarshravan@gmail.com>
2025-02-02 18:43:51 -0500

    Utilized the exchangeRate, amount, and amountInFromCurrency variables to properly have the conversion take place and also displayed in the input boxes

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,15 @@ function App() {
   const [exchangeRate, setExchangeRate] = useState()
   const [amount, setAmount] = useState(1)
   const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
-  console.log(exchangeRate)
+
+  let toAmount, fromAmount
+  if (amountInFromCurrency) {
+    fromAmount = amount
+    toAmount = amount * exchangeRate
+  } else {
+    toAmount = amount
+    fromAmount = amount / exchangeRate
+  }
 
 
   useEffect(() => {
@@ -36,12 +44,14 @@ function App() {
         currencyOptions={currencyOptions}
         selectedCurrency={fromCurrency}
         onChangeCurrency={e => setFromCurrency(e.target.value)}
+        amount={fromAmount}
       />
       <div className="equals">=</div>
       <CurrencyRow
         currencyOptions={currencyOptions}
         selectedCurrency={toCurrency}
         onChangeCurrency={e => setToCurrency(e.target.value)}
+        amount={toAmount}
       />
     </>
   );
